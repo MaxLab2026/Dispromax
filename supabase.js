915e9c0d0a36a5c8af4566981eaa4ce927eab69e
MaxLab2026 <carlos.cardenas.ucv@gmail.com>
2026-03-24 13:20:43 -0500

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-// supabase.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
-
-// Credenciales de tu proyecto
-const supabaseUrl = "https://cosxgumrxgihkeszvgwu.supabase.co"
-const supabaseKey = "sb_publishable_B58gh4o9vrHK58hNramtGw_yRck0jwX"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
